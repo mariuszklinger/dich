d37cad8363a833bc5404b3a2665ae0c7a5094276
Mariusz Klinger <mariusz@mariuszklinger.pl>
2014-06-01 01:13:41 +0100

    test tool classes - NodeConnector & NodeFactory

--- src/test/scala/pl/mariuszklinger/core/tools/NodeConnector.scala
+++ src/test/scala/pl/mariuszklinger/core/tools/NodeConnector.scala
@@ -1,5 +1,10 @@
 package pl.mariuszklinger.core.tools
 
+import pl.mariuszklinger.core.Node
+
 object NodeConnector {
 
+    def connect(n1: Node, n2: Node){
+        n1.connect("localhost", n2.port)
+    }
 }
--- src/test/scala/pl/mariuszklinger/core/tools/NodeFactory.scala
+++ src/test/scala/pl/mariuszklinger/core/tools/NodeFactory.scala
@@ -1,5 +1,42 @@
 package pl.mariuszklinger.core.tools
 
-class NodeFactory {
+import pl.mariuszklinger.core.Node
 
+import java.net.BindException
+
+object NodeFactory {
+
+    private val START_PORT = 10000
+
+    private val getNextPort = (() => {
+        var n = START_PORT
+        () => {
+            n += 1
+            n
+        }
+    })()
+
+    private val getNextNick = (() => {
+        var i = 0
+        val nicknames = Array("Luke", "Anakin", "Boba", "Leia", "Han", "Amidala")
+
+        () => {
+            i += 1
+            nicknames(i % nicknames.length)
+        }
+    })()
+
+    def getNode(): Node = {
+
+        do{
+            try{
+                return new Node(getNextNick(), getNextPort())
+            }
+            catch{
+                case e: BindException =>
+            }
+        } while(true)
+
+        null
+    }
 }
